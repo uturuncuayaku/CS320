5306e2a767af6a3d30f35146382049de9b285fd0
Uturuncuayaku <andtruj23@gmail.com>
2024-09-24 11:22:40 -0500

    Task service method structure created.

--- Module_Four/src/edu/snhu/task_services/TaskService.java
+++ Module_Four/src/edu/snhu/task_services/TaskService.java
@@ -1,5 +1,55 @@
+/**
+ * @Author Andres
+ * SNHU
+ * 9/29/2024
+ * Prof. Parul Hirpara
+ * 
+ */
+
 package edu.snhu.task_services;
 
-public class TaskService {
+import java.util.HashMap;
 
+public class TaskService {
+	
+	private HashMap<String, Task> task_map;
+	
+	public TaskService() {
+		task_map = new HashMap<>();
+	}
+	
+	public boolean addTasks(String id) {
+		if (task_map.containsKey(id)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean deleteTasks(String id) {
+		if (task_map.containsKey(id)) {
+			task_map.remove(id);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void updateTaskName(String id, String name) {
+		if (task_map.containsKey(id)) {
+			Task updateTask = task_map.get(id);
+			updateTask.setName(name);
+		}else {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	public void updateTaskDescription(String id, String desc) {
+		if (task_map.containsKey(id)) {
+			Task updateTask = task_map.get(id);
+			updateTask.setName(desc);
+		}else {
+			throw new IllegalArgumentException();
+		}
+	}
 }

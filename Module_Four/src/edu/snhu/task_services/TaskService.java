84d288537122f06d93e732252940280ffbc6848d
Uturuncuayaku <andtruj23@gmail.com>
2024-09-24 23:44:29 -0500

    Comments added

--- Module_Four/src/edu/snhu/task_services/TaskService.java
+++ Module_Four/src/edu/snhu/task_services/TaskService.java
@@ -5,38 +5,68 @@
  * Prof. Parul Hirpara
  * 
  */
-
 package edu.snhu.task_services;
 
 import java.util.HashMap;
 
+/*
+ * Our task service object contains a hash map implementing an in-memory database
+ * 
+ */
 public class TaskService {
 	
+	//Field members
 	private HashMap<String, Task> task_map;
 	
+	/*
+	 * Constructor 
+	 * Generates our hash map, that will act as our database.
+	 */
 	public TaskService() {
+		//Creates and initializes the database
 		task_map = new HashMap<>();
+		
 	}
 	
-	public boolean addTasks(String id) {
-		if (task_map.containsKey(id)) {
-			return true;
+	/*
+	 * Function to add a task to our database
+	 */
+	public void addTasks(String id, Task task) {
+		//Search through the hash map for a task with the id given
+		if (!task_map.containsKey(id)) {
+			//Place our task within the mapping of our unique id			
+			task_map.put(id, task);
+			
 		}else {
-			return false;
+			throw new IllegalArgumentException();
 		}
 	}
 	
-	public boolean deleteTasks(String id) {
+	/*
+	 * Function to delete a task
+	 * @throws IllegalArgumentException if there is no id to delete
+	 */
+	public void deleteTasks(String id) {
+		//Search for the key within the hash mapping.
 		if (task_map.containsKey(id)) {
+			//Remove the task if we find it.
 			task_map.remove(id);
-			return true;
+			
 		}else {
-			return false;
+			throw new IllegalArgumentException();
 		}
 	}
 	
+	/*
+	 * Function updates a task's name
+	 * Will set the name but first we must check if it exists
+	 * and we must also validate the name as well.
+	 * @throws	IllegalArgumentException 
+	 */
 	public void updateTaskName(String id, String name) {
+		//Search for the task.
 		if (task_map.containsKey(id)) {
+			//update the task name if found.
 			Task updateTask = task_map.get(id);
 			updateTask.setName(name);
 		}else {
@@ -44,8 +74,15 @@ public class TaskService {
 		}
 	}
 	
+	/*
+	 * Function that updates the description according the 
+	 * requirements for a description length.
+	 * @throws	IllegalArgumentException
+	 */
 	public void updateTaskDescription(String id, String desc) {
+		//Searches for the task.
 		if (task_map.containsKey(id)) {
+			//Updates the task description.
 			Task updateTask = task_map.get(id);
 			updateTask.setName(desc);
 		}else {

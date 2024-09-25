ebc457914eaf8a3aafe591898770a007f7e75658
Uturuncuayaku <andtruj23@gmail.com>
2024-09-25 00:21:06 -0500

    Created a task service test class.

---
+++ Module_Four/src/edu/snhu/tests/task_services/TaskServiceTest.java
@@ -0,0 +1,17 @@
+package edu.snhu.tests.task_services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.snhu.task_services.TaskService;
+import edu.snhu.task_services.Task;
+
+class TaskServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
